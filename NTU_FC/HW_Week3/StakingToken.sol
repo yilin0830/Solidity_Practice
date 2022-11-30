141532e156871296c0a0a6b3f47c0be8e454f888
Boralin <lsst50213@gmail.com>
2022-11-30 17:49:11 +0800

    HW_Week3

---
+++ NTU_FC/HW_Week3/StakingToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract testERC20 is ERC20 {
+    constructor() ERC20("testERC20", "tERC") {
+        // _mint(msg.sender, 10 ** (9 + 18));
+        _mint(msg.sender, 1_000_000_000 * 1e18);
+    }
+}
